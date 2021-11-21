1975f92959389be2864ae13c3904cd099959764a
devstorm321 <ddstorm0114@outlook.com>
2021-11-21 10:47:41 +0300

    5day pass cleanup

--- 5dayByPass.py
+++ 5dayByPass.py
@@ -1,11 +1,7 @@
-__author__ =  "enc0der"
-
 import sys
-import logging
-import xbmc, xbmcaddon, xbmcgui
+import xbmcaddon, xbmcgui
 import resources.modules.scraper as scraper
 
-logger = logging
 
 dialog = xbmcgui.Dialog()
 __settings__ = xbmcaddon.Addon(id="plugin.video.yams")
@@ -18,7 +14,8 @@ session = __settings__.getSetting(id="session")
 #digest = yamsutils.__digest(ADDON_PATH)
 digest = "0860588e3a23f9637ea47c3dc314a63185515d29bf7c839612a9e88ce596847b"
 
-scraper.__set_digest(digest)
+
+scraper.__set_digest( digest )
 
 if username == "" or username is None:
     dialog.ok("Oops", "Sorry Username is invalid/compulsory")
@@ -32,7 +29,9 @@ if dialog.yesno("Day Pass", "Would you like to activate day pass for 5 days"):
     bypass = scraper.get5DayBypass(username, password)
     print(bypass)
     if bypass["success"]:
-        dialog.ok("Success", "You are using {0} out of 3, 5 Day passes.".format(bypass["bypasses"]))
-        dialog.ok("Restart Required", "Please restart your device for day pass to take effect (shutdown icon -> exit)")
+        dialog.ok("Success", 
+            f'''You are using {0} out of 3, 5 Day passes.'''.format(bypass["bypasses"]))
+        dialog.ok("Restart Required", 
+            "Please restart your device for day pass to take effect (shutdown icon -> exit)")
     else:
         dialog.ok("Failure", bypass["reason"])
