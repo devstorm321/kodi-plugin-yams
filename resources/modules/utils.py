5b4cec3c781089fe831054d547f5c54592e3ed6d
devstorm321 <ddstorm0114@outlook.com>
2021-11-23 15:17:17 +0300

    cleanup: remove unused

--- resources/modules/utils.py
+++ resources/modules/utils.py
@@ -1,9 +1,5 @@
 import datetime as dt
-import os
 import requests
-
-import xbmc
-import xbmcgui
 
 
 def log(text):
@@ -16,118 +12,6 @@ class ApiError(Exception):
 
     def __str__(self):
         return self.exception
-
-
-def killXbmc():
-    platform = queryPlatform()
-
-    if platform == 'OSX':
-        try:
-            os.system('killall -9 XBMC')
-        except:
-            pass
-
-        try:
-            os.system('killall -9 Kodi')
-        except:
-            pass
-    elif platform == 'AppleTV':
-        try:
-            os.system('killall AppleTV')
-        except:
-            pass
-
-        try:
-            os.system('sudo initctl stop kodi')
-        except:
-            pass
-
-        try:
-            os.system('sudo initctl stop xbmc')
-        except:
-            pass
-    elif platform == 'Linux':
-        try:
-            os.system('killall XBMC')
-        except:
-            pass
-
-        try:
-            os.system('killall Kodi')
-        except:
-            pass
-
-        try:
-            os.system('killall -9 xbmc.bin')
-        except:
-            pass
-
-        try:
-            os.system('killall -9 kodi.bin')
-        except:
-            pass
-    #    elif (platform == 'Android'):
-    #        try:
-    #            os.system('adb shell am force-stop org.xbmc.kodi')
-    #        except:
-    #            pass
-    #
-    #        try:
-    #            os.system('adb shell am force-stop org.kodi')
-    #        except:
-    #            pass
-    #
-    #        try:
-    #            os.system('adb shell am force-stop org.xbmc.xbmc')
-    #        except:
-    #            pass
-    #
-    #        try:
-    #            os.system('adb shell am force-stop org.xbmc')
-    #        except:
-    #            pass
-    elif platform == 'Windows':
-        try:
-            os.system('@ECHO off')
-            os.system('tskill XBMC.exe')
-        except:
-            pass
-
-        try:
-            os.system('@ECHO off')
-            os.system('tskill Kodi.exe')
-        except:
-            pass
-
-        try:
-            os.system('@ECHO off')
-            os.system('TASKKILL /im XBMC.exe /f')
-        except:
-            pass
-
-        try:
-            os.system('@ECHO off')
-            os.system('TASKKILL /im Kodi.exe /f')
-        except:
-            pass
-    else:
-        dialog = xbmcgui.Dialog()
-        dialog.ok('AstreamWeb Maintenance', 'Please pull the power cable from your device.')
-
-
-def queryPlatform():
-    if xbmc.getCondVisibility('system.platform.osx'):
-        return 'OSX'
-    elif xbmc.getCondVisibility('system.platform.atv2'):
-        return 'AppleTV'
-    elif xbmc.getCondVisibility('system.platform.linux'):
-        return 'Linux'
-    elif xbmc.getCondVisibility('system.platform.android'):
-        return 'Android'
-    elif xbmc.getCondVisibility('system.platform.windows'):
-        return 'Windows'
-    else:
-        return 'Other'
 
 
 def postHtml(url, form_data={}, headers={}, compression=True, NoCookie=None):
